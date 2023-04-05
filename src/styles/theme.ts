37f58476003c7107642a69d21ee0ec1c62f2a178
YUZAMIN <untmd.crmn@gmail.com>
2023-04-05 17:12:44 +0900

    design: 전역으로 사용되는 색상 코드를 theme에 정의하라
    Fixes: #1

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,12 @@
-const color = {};
+const color = {
+  primary: '#FAA250',
+  secondary: '',
+  tertiary: '',
+  white: '#FFFFFF',
+  black: '#2A2A2A',
+  grey: '#999999',
+  lightGrey: '#D9D9D9',
+};
 
 const layout = {};
 
