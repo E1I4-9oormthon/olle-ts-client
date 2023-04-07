edf46d7905c0120a5fc54441f1be33a9af1675a4
YUZAMIN <untmd.crmn@gmail.com>
2023-04-08 02:39:46 +0900

    feat: WelcomePage 페이지 컴포넌트를 생성하라
    - 로고 이미지 하단에 카카오 로그인 이미지 버튼 삽입
    Fixes: #1

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,14 +1,44 @@
 import styled from 'styled-components';
+import TitleImg from 'assets/images/logo_full_img.png';
+import KakaoLoginButtonImg from 'assets/images/kakao_signin_button_img.png';
 
 export const WelcomePage = () => {
-  console.log(import.meta.env.VITE_KAKAO_AUTH_URL);
   return (
     <Wrapper>
-      <>
-        <a href={import.meta.env.VITE_KAKAO_AUTH_URL}>Kakao</a>
-      </>
+      <Box>
+        <TitleImage src={TitleImg} />
+        <a href={import.meta.env.VITE_KAKAO_AUTH_URL}>
+          <KakaoSignInButtonImage src={KakaoLoginButtonImg} />
+        </a>
+      </Box>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  height: 100%;
+  padding: 3rem 2rem;
+`;
+
+const Box = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TitleImage = styled.img`
+  width: 65%;
+  max-width: 260px;
+  min-width: 100px;
+  margin-bottom: 14rem;
+`;
+
+const KakaoSignInButtonImage = styled.img`
+  width: 100%;
+`;
