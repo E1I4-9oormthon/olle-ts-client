bc8bb8611ebe2541905cfff2ca771ab43f3884c6
YUZAMIN <untmd.crmn@gmail.com>
2023-04-08 02:37:34 +0900

    feat: 전역적으로 사용할 버튼 컴포넌트 구현
    Fixes: #1

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,54 @@
+import { memo } from 'react';
+import styled from 'styled-components';
+import { theme } from '../../styles/theme';
+
+interface ButtonProps {
+  handleClick: () => void;
+  name: string;
+  isActivated: boolean;
+}
+
+interface ButtonBoxProps {
+  isActivated: boolean;
+}
+
+export const Button = memo(({ handleClick, name, isActivated }: ButtonProps) => {
+  return (
+    <ButtonBox
+      onClick={
+        isActivated
+          ? handleClick
+          : () => {
+              return;
+            }
+      }
+      isActivated={isActivated}
+    >
+      {name}
+    </ButtonBox>
+  );
+});
+
+const ButtonBox = styled.div<ButtonBoxProps>`
+  padding: 1rem;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  transition: all 0.2s ease;
+  color: ${theme.color.white};
+  background: ${theme.color.primary};
+  ${({ isActivated }) =>
+    isActivated &&
+    `    
+  &:hover {
+    cursor: pointer;
+  }
+`}
+  ${({ isActivated }) =>
+    !isActivated &&
+    `    
+    background: ${theme.color.grey} !important;
+`}
+`;
