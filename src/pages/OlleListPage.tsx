60096fea6a10fe38d1ef15f17d01c2e60ea4e449
YUZAMIN <untmd.crmn@gmail.com>
2023-04-08 19:54:42 +0900

    feat: OlleListPage 페이지 컴포넌트를 생성하라
    Fixes: #8

---
+++ src/pages/OlleListPage.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const OlleListPage = () => {
+  return (
+    <Wrapper>
+      <Box>HELLO!</Box>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  height: 100%;
+  padding: 3rem 2rem;
+`;
+
+const Box = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
