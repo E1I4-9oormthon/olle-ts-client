fa88ea3bbe616beb092f5bdff3fd799dd4925b15
YUZAMIN <untmd.crmn@gmail.com>
2023-04-08 19:56:04 +0900

    feat: axios의 baseURL을 설정하라
    Fixes: #8

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from 'App';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
