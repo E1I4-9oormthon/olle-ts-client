225523aa9daa0c100de051f35aa27c4fe04d3b11
YUZAMIN <untmd.crmn@gmail.com>
2023-04-18 00:45:34 +0900

    feat: 동행 제안 폼에 동행 성별 입력 버튼 항목을 구현하라
    - 기존의 Box를 Form 요소로 변경
    Fixes: #10

--- src/pages/OlleWritePage.tsx
+++ src/pages/OlleWritePage.tsx
@@ -1,21 +1,68 @@
-import { Input } from 'components/Input';
+import { Input } from 'components/common/Input';
+import { BorderButton } from 'components/common/BorderButton';
 import styled from 'styled-components';
 
 export const OlleWritePage = () => {
   return (
     <Wrapper>
-      <Box>
-        <Input title="제목을 입력하세요" placeholder="동행 제목을 입력해주세요" />
-      </Box>
+      <OlleForm>
+        <InputTitle>제목을 입력하세요</InputTitle>
+        <Input placeholder="동행 제목을 입력해주세요" />
+        <InputTitle>동행자 성별을 선택해 주세요</InputTitle>
+        <ButtonContainer>
+          <ButtonWrapper>
+            <BorderButton
+              handleClick={() => console.log(1)}
+              name="여성"
+              nameAlign="center"
+              isClicked={true}
+              value={1}
+            />
+          </ButtonWrapper>
+          <ButtonWrapper>
+            <BorderButton
+              handleClick={() => console.log(1)}
+              name="남성"
+              nameAlign="center"
+              isClicked={true}
+              value={1}
+            />
+          </ButtonWrapper>
+          <ButtonWrapper>
+            <BorderButton
+              handleClick={() => console.log(1)}
+              name="상관없어요"
+              nameAlign="center"
+              isClicked={true}
+              value={1}
+            />
+          </ButtonWrapper>
+        </ButtonContainer>
+      </OlleForm>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   min-height: 100%;
-  padding: 3rem 2rem;
+  padding: 1rem 2rem 3rem;
 `;
 
-const Box = styled.div`
+const OlleForm = styled.form`
   width: 100%;
+`;
+
+const InputTitle = styled.div`
+  margin: 2rem 0 1rem;
+  font-size: 1.3rem;
+  font-weight: 600;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ButtonWrapper = styled.div`
+  width: calc(90% / 3);
 `;
