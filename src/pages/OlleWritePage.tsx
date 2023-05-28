66c83579f792f0660d6fadb1bbace79a58c557e9
YUZAMIN <untmd.crmn@gmail.com>
2023-05-28 23:40:39 +0900

    feat: 출발 및 도착 지점을 제어하는 route state를 생성하라
    Fixes: #10

--- src/pages/OlleWritePage.tsx
+++ src/pages/OlleWritePage.tsx
@@ -11,6 +11,7 @@ export const OlleWritePage = () => {
   const [gender, setGender] = useState<number>(0);
   const [date, setDate] = useState<Date | undefined>(new Date());
   const [course, setCourse] = useState<number>(0);
+  const [route, setRoute] = useState<{ lat: number; lng: number }[]>([{ lat: 0, lng: 0 }]);
 
   const handleGenderButtonClick = (value: number) => {
     setGender(value);
@@ -24,36 +25,9 @@ export const OlleWritePage = () => {
     setCourse(parseInt(e.target.value));
   };
 
-  const courseData: Option[] = [
-    { index: 1, name: '시흥 - 광치기 올레' },
-    { index: 2, name: '우도 - 올레' },
-    { index: 3, name: '광치기 - 온평 올레' },
-    { index: 4, name: '온평 - 표선 올레 A코스' },
-    { index: 5, name: '온평 - 표선 올레 B코스' },
-    { index: 6, name: '표선 - 남원 올레' },
-    { index: 7, name: '남원 - 쇠소깍 올레' },
-    { index: 8, name: '쇠소깍 - 제주올레 여행자센터 올레' },
-    { index: 9, name: '제주올레 여행자센터 - 월평 올레' },
-    { index: 10, name: '서귀포 버스터미널 - 제주올레 여행자센터 올레' },
-    { index: 11, name: '월평 - 대평 올레' },
-    { index: 12, name: '대평 - 화순 올레' },
-    { index: 13, name: '화순 - 모슬포 올레' },
-    { index: 14, name: '가파도 - 올레' },
-    { index: 15, name: '모슬포 - 무릉 올레' },
-    { index: 16, name: '무릉 - 용수 올레' },
-    { index: 17, name: '용수 - 저지 올레' },
-    { index: 18, name: '저지 - 서광 올레' },
-    { index: 19, name: '한림 - 고내 올레 A코스' },
-    { index: 20, name: '한림 - 고내 올레 B코스' },
-    { index: 21, name: '고내 - 광령 올레' },
-    { index: 22, name: '광령 - 제주원도심 올레' },
-    { index: 23, name: '제주원도심 - 조천 올레' },
-    { index: 24, name: '상추자 - 올레' },
-    { index: 25, name: '하추자 - 올레' },
-    { index: 26, name: '조천 - 김녕 올레' },
-    { index: 27, name: '김녕 - 하도 올레' },
-    { index: 28, name: '하도 - 종달 올레' },
-  ];
+  const updateRoute = (newRoute: { lat: number; lng: number }[]) => {
+    setRoute(newRoute);
+  };
 
   return (
     <Wrapper>
