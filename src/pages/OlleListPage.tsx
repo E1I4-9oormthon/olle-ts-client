c498f1f7a4b26ae984c96ba000904b0966c39844
YUZAMIN <untmd.crmn@gmail.com>
2023-06-28 21:47:38 +0900

    feat: 동행 제안서 목록 페이지에서 제안서 목록을 fetch하라
    Fixes: #12

--- src/pages/OlleListPage.tsx
+++ src/pages/OlleListPage.tsx
@@ -1,20 +1,46 @@
 import styled from 'styled-components';
+import { olle } from 'apis/olle';
+import { useEffect, useState } from 'react';
+import { CustomError, Olle } from 'global/types';
 
 export const OlleListPage = () => {
+  const [olleList, setOlleList] = useState<Olle[]>();
+  const fetchOlleList = async () => {
+    try {
+      const fetchedData = await olle.getOlleList();
+      const olleList = fetchedData.map((data: Olle) => ({
+        applies_count: data.applies_count,
+        contact: data.contact,
+        course: data.course,
+        nickname: data.nickname,
+        prefer_gender: data.prefer_gender,
+        route: data.route,
+        start_date: data.start_date,
+        title: data.title,
+      }));
+      setOlleList(olleList);
+    } catch (err) {
+      const error = err as CustomError;
+      alert(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchOlleList();
+  }, []);
+
+  console.log(olleList);
+
   return (
     <Wrapper>
-      <Box>HELLO!</Box>
+      <Box>{olleList && olleList.map((olle) => <div>{olle.title}</div>)}</Box>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
+  min-height: inherit;
   height: 100%;
-  padding: 3rem 2rem;
+  padding: 2rem;
 `;
 
 const Box = styled.div`
