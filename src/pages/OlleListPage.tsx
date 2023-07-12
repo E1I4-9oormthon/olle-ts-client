8066c549cfb082b0e240bc8632d6ffc26e36a482
YUZAMIN <untmd.crmn@gmail.com>
2023-07-12 16:20:18 +0900

    feat: 동행 제안서 목록 UI를 수정하라
    - Figma 디자인에 맞게 UI를 수정하여 표시
    Fixes: #12

--- src/pages/OlleListPage.tsx
+++ src/pages/OlleListPage.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { olle } from 'apis/olle';
 import { useEffect, useState } from 'react';
 import { CustomError, Olle } from 'global/types';
+import { theme } from 'styles/theme';
 
 export const OlleListPage = () => {
   const [olleList, setOlleList] = useState<Olle[]>();
@@ -12,11 +13,12 @@ export const OlleListPage = () => {
         applies_count: data.applies_count,
         contact: data.contact,
         course: data.course,
-        nickname: data.nickname,
         prefer_gender: data.prefer_gender,
         route: data.route,
         start_date: data.start_date,
         title: data.title,
+        olle_id: data.olle_id,
+        olle_writer: data.olle_writer,
       }));
       setOlleList(olleList);
     } catch (err) {
@@ -28,19 +30,52 @@ export const OlleListPage = () => {
     fetchOlleList();
   }, []);
 
-  console.log(olleList);
+  const preferGenderText = (preferGender: number) => {
+    switch (preferGender) {
+      case 0:
+        return <div>상관없어요</div>;
+      case 1:
+        return <div>여성분과 동행할래요</div>;
+      case 2:
+        return <div>남성분과 동행할래요</div>;
+      default:
+        return;
+    }
+  };
 
   return (
     <Wrapper>
-      <Box>{olleList && olleList.map((olle) => <div>{olle.title}</div>)}</Box>
+      <FavoriteSelectTitle>{'말동무와 도란도란'}</FavoriteSelectTitle>
+      <Box>
+        {olleList &&
+          olleList.map((olle) => (
+            <OlleWrapper key={olle.olle_id}>
+              <ProfileImage src={olle.olle_writer.profile_image_url} />
+              <ContentsWrapper>
+                <Title>{olle.title}</Title>
+                <Nickname>{olle.olle_writer.nickname}</Nickname>
+                <DetailedInfo>
+                  {preferGenderText(olle.prefer_gender)}
+                  <div> {olle.start_date.split('T')[0]}</div>
+                </DetailedInfo>
+              </ContentsWrapper>
+            </OlleWrapper>
+          ))}
+      </Box>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  min-height: inherit;
-  height: 100%;
-  padding: 2rem;
+  min-height: 100%;
+  padding: 1.5rem;
+  background: #f5f5f5;
+`;
+
+const FavoriteSelectTitle = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+  padding: 0.5rem 0;
 `;
 
 const Box = styled.div`
@@ -49,4 +84,47 @@ const Box = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+`;
+
+const OlleWrapper = styled.div`
+  width: 100%;
+  margin: 0.5rem 0;
+  background: ${theme.color.white};
+  border: 1px solid #e1e1e8;
+  border-radius: 10px;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const ProfileImage = styled.img`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  margin-right: 5px;
+`;
+const ContentsWrapper = styled.div`
+  width: 100%;
+  padding: 1rem 0.5rem;
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+`;
+
+const Nickname = styled.div`
+  font-size: 17px;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+  color: ${theme.color.grey};
+`;
+
+const DetailedInfo = styled.div`
+  display: flex;
+  font-size: 15px;
+  justify-content: space-between;
+  color: ${theme.color.grey};
 `;
